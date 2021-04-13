2aefc664d7cfa613b9c32b3d86cf343953a7024a
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-04-13 22:27:25 +0600

    working on register screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -21,13 +21,95 @@ class _RegisterScreenState extends State<RegisterScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                IconButton(icon: Icon(Icons.close_outlined), onPressed: () {}),
+                SizedBox(
+                  height: 20,
+                ),
+                Icon(Icons.close),
+                SizedBox(
+                  height: 15,
+                ),
                 Text(
                   "Register and Get",
                   style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.w500,
                       color: Colors.red),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  "Rs 2,501 Instantly\nIn Your Perperfry Wallet",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black),
+                ),
+                SizedBox(
+                  height: 7,
+                ),
+                Text(
+                  "These Credits Can Be Clubbed With Other Offes",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(hintText: "Name"),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(hintText: "Mobile"),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(hintText: "Email ID"),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(hintText: "Password"),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    InkWell(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterScreen())),
+                      child: Container(
+                        height: 50,
+                        width: 250,
+                        child: Center(
+                            child: Text("REGISTER",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                    fontSize: 18))),
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          colors: [
+                            Colors.red,
+                            Colors.orange,
+                          ],
+                        )),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
