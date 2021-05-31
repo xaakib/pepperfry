cc6b12d8d8e57112c54addac582a4ff39e56b33e
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-05-31 18:31:40 +0600

    api is clearly

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -16,6 +16,7 @@ class ApiServices {
       print("apiServices : $data");
       return productsModelFromJson(data);
     } else {
+      print("apiServices : ${response.body}");
       return null;
     }
   }
